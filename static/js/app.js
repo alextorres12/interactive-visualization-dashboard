e933719685a82fc2531e6a199640a10bda1fbb80
Alex Torres <62618236+alextorres12@users.noreply.github.com>
2021-02-18 14:55:48 -0500

    saving first mapping test

--- static/js/app.js
+++ static/js/app.js
@@ -3,8 +3,23 @@ const json = "../data/samples.json"
 // Button handler
 function optionChanged(subjectID) {
 
-    console.log("new subject selected ", subjectID)
+    console.log("new subject selected ", subjectID);
     
-
-
 }
+
+function init() {
+    // Get dropdown menu item
+    var dropdownMenu = d3.select("#selDataset");
+
+    // Get list of Subject IDs from JSON file and add to dropdown menu
+    d3.json(json).then(function(data){
+        var subjects = data.map(subject => subject["metadata"].id);
+
+        console.log(subjects);
+
+        
+        
+    })
+}
+
+init();
