cb679e87865f713a1840baa51ea1f81ff5b8f8ff
Alex Torres <62618236+alextorres12@users.noreply.github.com>
2021-02-18 15:24:00 -0500

    working initial bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -28,9 +28,44 @@ function init() {
 function buildPlots(subjectID){
     // Get information from JSON file and use it to build the plots
     d3.json(json).then(function(data){
+
         // assign data to variables
         var samples = data["samples"].filter(sample => sample.id == subjectID);
+        
         console.log(samples);
+
+        var otuIDs = samples[0].otu_ids;
+        var otuLabels = samples[0].otu_labels;
+        var sampleValues = samples[0].sample_values;
+
+        // Build Bar Chart
+        // Get top 10 code from https://stackoverflow.com/questions/34883068/how-to-get-first-n-number-of-elements-from-an-array
+        var top10IDs = otuIDs.slice(0,10).map(id => `OTU ${id}`);
+        var top10Samples = sampleValues.slice(0,10);
+        var top10Labels = otuLabels.slice(0,10);
+
+        var trace1 = {
+            y: top10IDs,
+            x: top10Samples,
+            text: top10Labels,
+            type: "bar",
+            orientation: "h"
+        };
+        
+        var data1 = [trace1];
+
+        var layout1 = {
+            title: `Top 10 Bacteria Types: Subject ${subjectID}`
+        };
+
+        Plotly.newPlot("bar", data1, layout1)
+
+
+
+
+
+
+
 
 
 
